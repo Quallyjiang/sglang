c0d55271ebe727f3c781f624cc31187cb1d276fa
Qually Jiang <qually.jiang@intel.com>
2025-06-20 12:57:55 +0000

    [feat] refactor forward methods for EPMoE and DeepseekV2 to reduce CPU gap

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -999,17 +999,16 @@ class EPMoESparseCPUInfer(EPMoESparseCPUInterface):
             )
             return self.adhoc_cpu_result
 
-    def forward_sync(self, hidden_states_shape, hidden_states_device, cpu_result):
+    def forward_sync(self, hidden_states_device):
+        self.cpu_infer.sync_with_cuda_stream(
+            torch.cuda.current_stream(hidden_states_device).cuda_stream
+        )
+
+    def forward_to_gpu(self, hidden_states_shape, hidden_states_device, cpu_result):
         bs = hidden_states_shape[0]
         if torch.cuda.is_current_stream_capturing() or bs <= self.cached_tensors_size:
-            self.cpu_infer.sync_with_cuda_stream(
-                torch.cuda.current_stream(hidden_states_device).cuda_stream
-            )
             return cpu_result.to(hidden_states_device, non_blocking=True)
         else:
-            self.cpu_infer.sync_with_cuda_stream(
-                torch.cuda.current_stream(hidden_states_device).cuda_stream
-            )
             return self.adhoc_gpu_result.copy_(cpu_result, non_blocking=True)
 
     def forward(self):
@@ -1186,7 +1185,10 @@ class EPMoEHeto(EPMoESparse):
         gpu_result = self.forward_routed_experts_maybe_gpu(
             hidden_states, router_logits  # here hidden_states will be destroyed
         )
-        result = self.forward_routed_experts_sync(
+        self.forward_routed_experts_sync(
+            hidden_states_device,
+        )
+        result = self.forward_routed_experts_combine(
             hidden_states_shape,
             hidden_states_device,
             hidden_states_dtype,
@@ -1223,6 +1225,13 @@ class EPMoEHeto(EPMoESparse):
 
     def forward_routed_experts_sync(
         self,
+        hidden_states_device,
+    ):
+        if self.cpu_moe:
+            self.cpu_moe.forward_sync(hidden_states_device)
+
+    def forward_routed_experts_combine(
+        self,
         hidden_states_shape,
         hidden_states_device,
         hidden_states_dtype,
@@ -1230,7 +1239,7 @@ class EPMoEHeto(EPMoESparse):
         cpu_result,
     ):
         if self.cpu_moe:
-            cpu_result_on_gpu = self.cpu_moe.forward_sync(
+            cpu_result_on_gpu = self.cpu_moe.forward_to_gpu(
                 hidden_states_shape, hidden_states_device, cpu_result
             )
 
