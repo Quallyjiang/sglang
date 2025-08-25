a4f787e5318cbfc5a26e6d0ccc51b9388c608315
Qually <qually.jiang@intel.com>
2025-08-25 17:33:50 +0000

    fix: qwen3 execution issue

--- python/sglang/srt/models/qwen3_moe.py
+++ python/sglang/srt/models/qwen3_moe.py
@@ -192,7 +192,9 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
         final_hidden_states = self.experts(
             hidden_states=hidden_states, router_logits=router_logits
         )
-        final_hidden_states = final_hidden_states[0]
+        if global_server_args_dict["enable_ep_moe_heto"]:
+            # EPMoEHeto will return a tuple instead
+            final_hidden_states = final_hidden_states[0]
 
         if self.tp_size > 1:
             final_hidden_states = tensor_model_parallel_all_reduce(final_hidden_states)
